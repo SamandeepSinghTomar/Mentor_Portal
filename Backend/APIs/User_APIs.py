281390fd66eaa5def862dd40366e9440d91d6d40
Samandeep Singh Tomar <50411635+SamandeepSinghTomar@users.noreply.github.com>
2022-10-28 18:10:24 +0530

    Environment updated and APIs documented with comments within file.

--- Backend/APIs/User_APIs.py
+++ Backend/APIs/User_APIs.py
@@ -1,17 +1,64 @@
 from flask_restful import Resource
 from requests import delete
-from . import model
+import models
 
+
+#USER_Api to deal with all information regarding user.
 class User_API(Resource):
+    #Create user using post request
+    #Body:{'user_name':'',
+    #      'password':'',
+    #      'first_name':'',
+    #      'middle_name':'',
+    #      'last_name':'',
+    #      'dob':'',
+    #      'email':'',
+    #      'roll_number':'',
+    #      'role':''}
     def post(self):
-        pass
-    def get(self,user_name):
-        pass
-    def put(self):
-        pass
-    def delete(self):
+        #response 
+        #status: 201, {'message':'User Created Successfully!'}
+        #raise_error: U101, {'message':'Email address already registered!!'}
+        #raise_error: U102, {'message':'User_name already taken!!'}
         pass
 
+    
+    #Give information about the user,
+    #body:{'user_name':'','password':''}
+    def get(self):
+        #response:
+        #status: 201,
+        #JSON {'token':'',
+        #      'user_name':''       #to be used in display of username in routed view
+        #      'first_name':''
+        #      'middle_name':'',
+        #      'last_name':'',
+        #      'dob':'',
+        #      'email':'',
+        #      'roll_number':'',
+        #      'role':''}
+        #raise_error: L101, {'message':'Credentials not matched!'}
+        pass
+    
+    
+    #To update profile or prefrences of user,
+    #body {'password':'','changed_field':'value_to_be_updated',---}
+    def put(self):
+        #response:
+        #status: 201, {'message':'Profile/Preferences updated successfully!'}
+        #raise_error: U201, {'message':'Password Incorrect!!'} 
+        pass
+    
+    
+    #Delete User (Optional to Implement)
+    def delete(self):
+        #response:
+        #status: 201, {'message':'User Deleted Successfully!'}
+        #raise_error: U201, {'message':'Password Incorrect!!'}
+        pass
+
+
+#Post_Api to deal with the posts created by user.
 def Post_API(Resource):
     def post(self):
         pass
--- Backend/models.py
+++ Backend/models.py
@@ -1,3 +1,4 @@
+from click import password_option
 from flask_sqlalchemy import SQLAlchemy
 
 db=SQLAlchemy()
@@ -5,20 +6,21 @@ db=SQLAlchemy()
 
 class User(db.Model):
     __tablename__='User'
-    user_id=db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_name=db.Column(db.String(100))
-    first_name=db.Column(db.String(40))
-    middle_name=db.Column(db.String(40))
-    last_name=db.Column(db.String(40))
-    dob=db.Column(db.DateTime)
-    email=db.Column(db.VARCHAR(60))
-    roll_number=db.Column(db.String(40))
-    role=db.Column(db.String)
-    created_at=db.Column(db.DateTime, default=db.func.now())
+    user_id     =       db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_name   =       db.Column(db.String(100))
+    password    =       db.Column(db.String(200))
+    first_name  =       db.Column(db.String(40))
+    middle_name =       db.Column(db.String(40))
+    last_name   =       db.Column(db.String(40))
+    dob         =       db.Column(db.DateTime)
+    email       =       db.Column(db.VARCHAR(60))
+    roll_number =       db.Column(db.String(40))
+    role        =       db.Column(db.String)
+    created_at  =       db.Column(db.DateTime, default=db.func.now())
 
 class Post(db.Model):
     __tablename__='Post'
-    user_id=db.Column(db.ForeignKey('User.user_id'),primary_key=True)
-    text=db.Column(db.BLOB)
-    created_at=db.Column(db.DateTime, default=db.func.now())
-    likes=db.Column(db.Integer)
+    user_id     =   db.Column(db.ForeignKey('User.user_id'),primary_key=True)
+    text        =   db.Column(db.BLOB)
+    created_at  =   db.Column(db.DateTime, default=db.func.now())
+    likes       =   db.Column(db.Integer)
